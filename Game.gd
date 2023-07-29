e94423d8bf95ad6d49a6693bfd3acc90158673de
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-07-29 20:30:09 +0200

    Parallax modifications

--- Game.gd
+++ Game.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+
+@onready var background: Sprite2D = $ParallaxBackground/ParallaxLayer/Background
+@onready var background_stars: Sprite2D = $ParallaxBackground/ParallaxLayer2/Stars1
+@onready var player: PlayerShip = Globals.player
+
+
+func _process(_delta: float) -> void:
+	background.position = floor(player.position / 500) * 500
+	background_stars.position = floor(player.position / 1000) * 1000
