f50a0d720c6d6a6aa20453ac6856930886a96328
WekufuStudios <wekufu.studios@gmail.com>
2023-07-29 14:45:24 -0400

    paralaje actualizado, segundo plano de estrellas

--- Game.gd
+++ Game.gd
@@ -3,9 +3,11 @@ extends Node2D
 
 @onready var background: Sprite2D = $ParallaxBackground/ParallaxLayer/Background
 @onready var background_stars: Sprite2D = $ParallaxBackground/ParallaxLayer2/Stars1
+@onready var background_stars2: Sprite2D = $ParallaxBackground/ParallaxLayer3/Stars2
 @onready var player: PlayerShip = Globals.player
 
 
 func _process(_delta: float) -> void:
 	background.position = floor(player.position / 500) * 500
 	background_stars.position = floor(player.position / 1000) * 1000
+	background_stars2.position = floor(player.position / 1000) * 1000
